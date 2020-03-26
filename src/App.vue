b2543f05625d798ba227d9107b84c801a2aafd69
jczabator <jczabator@gmail.com>
2020-03-26 03:16:48 +0100

    Add CurrentDate component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app">    
+    <HelloWorld msg="Hello Vue!"/>    
+    <CurrentDate message="You loaded this page on "/>
+  </div>     
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CurrentDate from './components/CurrentDate.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CurrentDate
   }
 }
 </script>
---
+++ src/components/CurrentDate.vue
@@ -0,0 +1,15 @@
+<template>
+  <span v-bind:title="message + new Date().toLocaleString()">
+    Hover your mouse over me for a few seconds
+    to see my dynamically bound title!
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'CurrentDate',
+  props: {
+    message: String
+  }
+}
+</script>
